a940c3559593030b102774716e63d43991dfdf3a
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-06-27 23:14:23 -0600

    add basic state and types

--- lib/tictactwo_web/live/room_controller_live.ex
+++ lib/tictactwo_web/live/room_controller_live.ex
@@ -1,7 +1,23 @@
 defmodule TictactwoWeb.RoomControllerLive do
+
+  @type player() :: :blue | :orange
+  @type gobbler() :: :xl | :large | :medium | :small | :xs | :premie
+  @type cell() :: %{
+    occupied_by: player() | nil,
+    pieces: [gobbler()] | nil
+  }
+
   use TictactwoWeb, :live_view
 
   def mount(_params, session, socket) do
+    socket =
+      socket
+      |> assign(
+        roomid: session["roomid"],
+        cells: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        player_turn: :owner,
+        selected_gobbler: nil
+      )
     {:ok, assign(socket, roomid: session["roomid"])}
   end
 
