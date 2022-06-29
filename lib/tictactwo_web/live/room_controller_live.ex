3c3dfc2a715dd2901a05ca325fe629230d2fe4c3
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-06-28 23:22:04 -0600

    add more state

--- lib/tictactwo_web/live/room_controller_live.ex
+++ lib/tictactwo_web/live/room_controller_live.ex
@@ -15,13 +15,21 @@ defmodule TictactwoWeb.RoomControllerLive do
       |> assign(
         roomid: session["roomid"],
         cells: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        player_turn: :owner,
-        selected_gobbler: nil
+        player_turn: :blue,
+        selected_gobbler: nil,
+        blue_gobblers_played: [],
+        blue_gobblers: gobblers(),
+        orange_gobblers_played: [],
+        orange_gobblers: gobblers()
       )
     {:ok, assign(socket, roomid: session["roomid"])}
   end
 
   def render(assigns) do
     TictactwoWeb.RoomView.render("show.html", assigns)
+  end
+
+  defp gobblers() do
+    [:xl, :large, :medium, :small, :xs, :premie]
   end
 end
