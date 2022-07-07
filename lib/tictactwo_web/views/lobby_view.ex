0403817f0a13d48399c9d84f39dd1a1c8f2fa1e7
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-07-06 23:15:12 -0600

    lobby styles

--- lib/tictactwo_web/views/lobby_view.ex
+++ lib/tictactwo_web/views/lobby_view.ex
@@ -1,3 +1,9 @@
 defmodule TictactwoWeb.LobbyView do
   use TictactwoWeb, :view
+
+  defp filter_self(users, current_username) do
+    Enum.filter(users, fn {_user_id, user_data} -> user_data.username != current_username end)
+  end
+
+  defp show_username(%{username: username}), do: username
 end
