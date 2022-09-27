5f339a01ba7777286bcb43a354799ae5c8d1f22d
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-09-26 22:04:02 -0600

    feat: add player component

---
+++ lib/tictactwo_web/views/components/player.ex
@@ -0,0 +1,26 @@
+defmodule TictactwoWeb.Components.Player do
+  use Phoenix.Component
+
+  import TictactwoWeb.RoomView
+
+  alias TictactwoWeb.Components.Gobbler
+
+  def info(assigns) do
+    ~H"""
+      <div>
+        <div class="flex justify-between">
+          <span>
+            <%= show_player_name(@game, @color) %> 
+          </span>
+        </div>
+        <Gobbler.list
+          game={@game}
+          current_user={@current_user}
+          display_user={@display_user}
+          color={@color}
+          class="row-start-1 row-end-2"
+        />
+      </div>
+    """
+  end
+end
