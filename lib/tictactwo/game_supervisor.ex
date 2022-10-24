d1993306021cb45a200f5e8d4162cdc4c53028cd
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-10-23 21:57:41 -0600

    feat: register game supervisor

--- lib/tictactwo/game_supervisor.ex
+++ lib/tictactwo/game_supervisor.ex
@@ -1,10 +1,10 @@
 defmodule Tictactwo.GameSupervisor do
-  alias Tictactwo.{TimeKeeper, GameManager}
+  alias Tictactwo.{TimeKeeper, GameManager, GameRegistry}
 
   use Supervisor
 
   def start_link(game) do
-    Supervisor.start_link(__MODULE__, game, name: String.to_atom("GameSupervisor_#{game.slug}"))
+    Supervisor.start_link(__MODULE__, game, name: GameRegistry.via("GameSupervisor_#{game.slug}"))
   end
 
   @impl true
