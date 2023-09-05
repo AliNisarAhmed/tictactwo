8a3471e31ae91a64a0c50053317a4a6e6b66e4b9
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2023-09-04 21:57:49 -0600

    feat: abort and resign icon buttons

--- lib/tictactwo_web/views/components/button.ex
+++ lib/tictactwo_web/views/components/button.ex
@@ -2,126 +2,151 @@ defmodule TictactwoWeb.Components.Button do
   use Phoenix.Component
 
   import PetalComponents.Button
+  alias Phoenix.LiveView.JS
+  alias PetalComponents.HeroiconsV1
 
-  attr :user_id, :integer, required: true
-  attr :user_data, :map, required: true
+  attr(:user_id, :integer, required: true)
+  attr(:user_data, :map, required: true)
 
   def challenge(assigns) do
     ~H"""
-      <.button
-          color="primary"
-          variant="outline"
-          size="lg"
-          phx-click="challenge-user"
-          phx-value-userid={@user_id}
-      >
-        <%= if @user_data.status == :challenge_sent do %>
-          Cancel?
-        <% else %> 
-          Challenge
-        <% end %>
-      </.button>
+    <.button
+      color="primary"
+      variant="outline"
+      size="lg"
+      phx-click="challenge-user"
+      phx-value-userid={@user_id}
+    >
+      <%= if @user_data.status == :challenge_sent do %>
+        Cancel?
+      <% else %>
+        Challenge
+      <% end %>
+    </.button>
     """
   end
 
-  attr :challenge, :map, required: true
-  attr :text, :string, required: true
+  attr(:challenge, :map, required: true)
+  attr(:text, :string, required: true)
 
   def accept_challenge(assigns) do
     ~H"""
-      <.button
-          color="success"
-          variant="outline"
-          phx-click="accept-challenge"
-          phx-value-challenger-username={@challenge.username}
-          phx-value-challenger-id={@challenge.id}
-      >
-        <%= @text %>
-      </.button>
+    <.button
+      color="success"
+      variant="outline"
+      phx-click="accept-challenge"
+      phx-value-challenger-username={@challenge.username}
+      phx-value-challenger-id={@challenge.id}
+    >
+      <%= @text %>
+    </.button>
     """
   end
 
-  attr :challenge, :map, required: true
-  attr :text, :string, required: true
+  attr(:challenge, :map, required: true)
+  attr(:text, :string, required: true)
 
   def reject_challenge(assigns) do
     ~H"""
-      <.button
-          color="danger"
-          variant="outline"
-          phx-click="reject-challenge"
-          phx-value-challenger-username={@challenge.username}
-          phx-value-challenger-id={@challenge.id}
-      >
-        <%= @text %>
-      </.button>
+    <.button
+      color="danger"
+      variant="outline"
+      phx-click="reject-challenge"
+      phx-value-challenger-username={@challenge.username}
+      phx-value-challenger-id={@challenge.id}
+    >
+      <%= @text %>
+    </.button>
     """
   end
 
   def accept_rematch(assigns) do
     ~H"""
-      <.button 
-        color="success"
-        phx-click="rematch-accepted"
-      >
-        Accept Rematch
-      </.button>
+    <.button color="success" phx-click="rematch-accepted">
+      Accept Rematch
+    </.button>
     """
   end
 
-  attr :current_user, :map, required: true
-  attr :user_type, :atom, required: true, values: [:blue, :orange, :spectator]
+  attr(:current_user, :map, required: true)
+  attr(:user_type, :atom, required: true, values: [:blue, :orange, :spectator])
 
   def offer_rematch(assigns) do
     ~H"""
-      <.button 
-        color="primary"
-        phx-click="offer-rematch"
-        phx-value-username={@current_user.username}
-        phx-value-color={@user_type}
-      >
-        Rematch
-      </.button>
+    <.button
+      color="primary"
+      phx-click="offer-rematch"
+      phx-value-username={@current_user.username}
+      phx-value-color={@user_type}
+    >
+      Rematch
+    </.button>
     """
   end
 
-  attr :current_user, :map, required: true
+  attr(:current_user, :map, required: true)
 
   def abort_game(assigns) do
     ~H"""
-      <.button 
-        color="danger"
-        phx-click="abort-game"
-        phx-value-username={@current_user.username}
-      >
-        Abort Game
-      </.button>
+    <.button
+      id="abort-button-client"
+      color="danger"
+      variant="outline"
+      phx-click={JS.hide() |> JS.show(to: "#abort-button")}
+      phx-value-username={@current_user.username}
+      class="border-0"
+    >
+      <HeroiconsV1.Outline.x class="w-6 h-6" />
+    </.button>
+    <.button
+      id="abort-button"
+      color="danger"
+      variant="outline"
+      phx-click="abort-game"
+      phx-click-away={JS.hide() |> JS.show(to: "#abort-button-client")}
+      phx-value-username={@current_user.username}
+      class="hidden border-0 bg-orange-500"
+    >
+      <HeroiconsV1.Outline.x class="w-6 h-6 fill-orange stroke-white" />
+    </.button>
     """
   end
 
-  attr :current_user, :map, required: true
+  attr(:current_user, :map, required: true)
 
   def resign_game(assigns) do
     ~H"""
-      <.button 
-        color="danger"
-        variant="outline"
-        phx-click="resign-game"
-        phx-value-username={@current_user.username}
-      >
-        Resign Game
-      </.button>
+    <.button
+      id="resign-button-client"
+      color="danger"
+      variant="outline"
+      phx-click={
+        JS.hide()
+        |> JS.show(to: "#resign-button")
+      }
+      class="border-0"
+    >
+      <HeroiconsV1.Outline.flag id="resign-icon" class="w-6 h-6" />
+    </.button>
+    <.button
+      id="resign-button"
+      color="danger"
+      variant="outline"
+      phx-click="resign-game"
+      phx-click-away={JS.hide() |> JS.show(to: "#resign-button-client")}
+      phx-value-username={@current_user.username}
+      class="hidden border-0 bg-orange-500"
+    >
+      <HeroiconsV1.Outline.flag id="resign-icon-full" class="w-6 h-6 fill-orange stroke-white" />
+    </.button>
     """
   end
 
   def back_to_lobby(assigns) do
     ~H"""
-      <.button 
-        color="info"
-        phx-click="back-to-lobby"
-      >
-        Back to Lobby
-      </.button>
+    <.button color="info" phx-click="back-to-lobby">
+      Back to Lobby
+    </.button>
     """
   end
 end
